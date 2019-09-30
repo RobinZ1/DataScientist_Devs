f9ddc49903cef0d8e323e73e8dbb7fe8cd074518
Luobin Zeng  (Robin Z) <41602694+RobinZ1@users.noreply.github.com>
2019-09-29 22:04:31 -0400

    Add files via upload

--- DataScience.py
+++ DataScience.py
@@ -33,3 +33,43 @@ result = df[any_bool].head()
 df.loc[df["column of values"] == 0, "column of values"] = np.nan
 column_after = df["column of values"].values_count(dropna=False).head()
 
+
+
+---------
+Sep 29
+
+USING ILOC[] TO SELECT BY INTEGER POSITION
+
+# Selecting a value:
+third_row_first_col = df.iloc[2,0]
+# Selecting a row:
+second_row = df.iloc[1]
+
+CREATING BOOLEAN MASKS USING PANDAS METHOD
+
+# selecting only null values in a column
+rev_is_null = f500["revenue_change"].isnull()
+# Filtering using Boolean series object:
+rev_change_null = f500[rev_is_null]
+# Selecting only the non-null values in a column:
+f500[f500["previous_rank"].notnull()]
+
+BOOLEAN OPERATORS
+
+#Multiple required filtering criteria:
+filter_big_rev_neg_profit = (f500["revenues"]>100000) & (f500["profits"]<0)
+#Multiple optional filtering criteria:
+filter_big_rev_neg_profit = (f500["revenues"]>100000) | (f500["profits"],0)
+
+CONCEPTS
+
+# To select values by axis labels, use loc[]. To select values by 
+# integer locations, use iloc[]   When the label for an axis is 
+# just its integer positions, these methods can be used intercahngeable
+
+# Because using a loop doesn't take advantage of vectorization, it's 
+# important to avoid doing so unless you absolutely have to. Boolean 
+# operators are a powerful technique to take advantage of vectorization
+# when filtering for expressing more granular filters
+
+
